3c491a1edc4f188a1e029e27aa60ff70a1ea747f
Tristan Cheah <cheahtristan@hotmail.sg>
2019-09-13 17:16:09 +0800

    Revert "Revert "merge""
    This reverts commit 3bffd42485ec94176850f02ef7b37473d40e97de.

--- Capstone Test/Assets/Scripts/move.cs
+++ Capstone Test/Assets/Scripts/move.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    private Rigidbody2D rb2d_player;
+    public Rigidbody2D rb2d_player;
+    public float y;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +16,42 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+
         if (name == "Player 1")
         {
-            rb2d_player.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            rb2d_player.velocity = new Vector3(0, yAxis, 0);
         }
         
         if (name == "Player 2")
         {
             rb2d_player.velocity = new Vector2(Input.GetAxisRaw("Horizontal_2"), Input.GetAxisRaw("Vertical_2"));
         }
+
+        /*if (Input.GetButtonDown("a"))
+        {
+            y++;
+        }
+
+        if (Input.GetButtonDown("d"))
+        {
+            y--;
+        }*/
+
+        y = xAxis * 3;
+        Rotate(y);
+    }
+
+    void Rotate(float y)
+    {
+        //transform.Rotate(new Vector3(0f, 0f, y));
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z += Input.GetAxisRaw("Horizontal") * -2f;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+
+        //Vector3 pos = 
     }
 }
